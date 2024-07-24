429d1e8d709665a0b7bdb9ff770743e0f3b2f0be
Helmi Ibrahim <helmi03@users.noreply.github.com>
2024-07-24 20:38:22 +0800

    sync schema 2024-07-22.0 (#35)
    * sync schema 2024-07-22.0
    
    * removed comments for old layers

--- overturemaps/core.py
+++ overturemaps/core.py
@@ -97,13 +97,12 @@ def geoarrow_schema_adapter(schema: pa.Schema) -> pa.Schema:
 
 
 type_theme_map = {
-    "locality": "admins",
-    "locality_area": "admins",
-    "administrative_boundary": "admins",
+    "address": "addresses",
     "building": "buildings",
     "building_part": "buildings",
     "division": "divisions",
     "division_area": "divisions",
+    "division_boundary": "divisions",
     "place": "places",
     "segment": "transportation",
     "connector": "transportation",
